bf189b11b5a6a59585f31c09eded0e1b1a48a12a
yalechen <yalechen@tencent.com>
2021-11-09 15:57:25 +0800

    init

---
+++ bin/django-admin.py
@@ -0,0 +1,4 @@
+#!/Users/anakinliu/codedog/codedog_puppy/data/tools/Python-v3.7.0/bin/python
+# EASY-INSTALL-SCRIPT: 'Django==2.1','django-admin.py'
+__requires__ = 'Django==2.1'
+__import__('pkg_resources').run_script('Django==2.1', 'django-admin.py')
